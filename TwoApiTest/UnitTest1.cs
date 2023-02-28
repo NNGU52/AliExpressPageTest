1147dd08a8a906a290f48ea5318fbf4caffb180f
Elena <elena@debian.GES>
2023-02-28 05:44:25 -0500

    Successfully tested the first test

---
+++ TwoApiTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using RA;
+using RestAssured;
+using ClassLibrary1;
+using RestSharp;
+using System;
+using System.Text.RegularExpressions;
+
+namespace TwoApiTest
+{
+    [TestFixture]
+
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            var library = new Class1();
+            var response = library.GetUsers();
+
+            // проверка, на то, что имена файлов-аватаров пользователей совпадают
+            for (int i = 0; i < response.data.Count; i++)
+            {
+                // проверка, с использованием ренгулярного выражения
+                Assert.AreEqual(Convert.ToInt32(Regex.Replace(response.data[i].avatar, @"[^\d]+", "")), response.data[i].id, "Error");
+                // порверка на наличие id в email с омощью метода Contains
+                Assert.IsTrue(response.data[i].avatar.Contains(response.data[i].id.ToString()));
+            }
+
+            for (int i = 0; i < response.data.Count; i++)
+            {
+                Assert.IsTrue(response.data[i].email.EndsWith("reqres.in"));
+            }
+        }
+    }
+}
